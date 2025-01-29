1687e78a12fcc0fade6f477cbadc280f8727c35c
sieun-p <78068899+sieun-p@users.noreply.github.com>
2025-01-29 21:11:14 +0900

    Title: 좌우로 움직이는 로봇, Time: 49ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250129/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -0,0 +1,27 @@
+MAX_iter = 100000
+n, m = map(int,input().split())
+loc_a = [0]*(MAX_iter+1)
+loc_b = [0]*(MAX_iter+1)
+
+time_a = 1
+for _ in range(n):
+    t, d = tuple(input().split())
+    for _ in range(int(t)):
+        if d=='R': loc_a[time_a] = loc_a[time_a-1]+1
+        else: loc_a[time_a] = loc_a[time_a-1]-1
+        time_a += 1
+time_b = 1
+for _ in range(m):
+    t, d = tuple(input().split())
+    for _ in range(int(t)):
+        if d=='R': loc_b[time_b] = loc_b[time_b-1]+1
+        else: loc_b[time_b] = loc_b[time_b-1]-1
+        time_b += 1
+
+cnt = 0
+loc_ab = 0
+for a,b in zip(loc_a, loc_b):
+    if a==b and a!=loc_ab:
+        cnt+=1
+        loc_ab = a
+print(cnt-1)
